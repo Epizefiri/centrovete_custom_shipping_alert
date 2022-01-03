cccbd41256631342cdc4156840b2baa6694b2f1b
Epizefiri <salvatore.dagostino@gmail.com>
2022-01-03 10:10:30 +0100

    Initial commit

---
+++ centrovete_custom_shipping_alert.php
@@ -0,0 +1,15 @@
+<?php
+ 
+/*
+  Plugin Name: Centrovete Custom Shipping Alert
+  Plugin URI: https://centrovete.it
+  Description: Simple Price Shipping Alert
+  Version: 1.0
+  Author: Salvatore 
+ */
+
+function centrovete_shipping_alert(){
+    echo 'd';
+}
+
+add_action('wp_head','centrovete_shipping_alert', 10);
